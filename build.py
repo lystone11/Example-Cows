0f6dd5547d1bb3ef4b971f88a799f4e73f84b5dd
Lydia Ashley <asong372@gmail.com>
2022-09-19 14:58:29 -0700

    ah

--- build.py
+++ build.py
@@ -10,6 +10,7 @@ def run(cmd):
     if (system(cmd) != 0):
         raise RuntimeError("Command failed")
 
+
 run("clang++ -Wall -Wextra -pedantic -c -std=c++17 cow.cpp")
 run("clang++ -Wall -Wextra -pedantic -c -std=c++17 main.cpp")
 run("clang++ -std=c++17 -o main main.o cow.o")
